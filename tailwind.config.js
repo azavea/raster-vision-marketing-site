498193533e5c7013b9fa77da0eb6b94d2f8d516d
Katie <kbrown@azavea.com>
2022-11-10 11:59:38 -0500

    Add default colors, fonts, buttons, and other styles
    This commit adds the default colors, fonts, font styles, and buttons
    laid out in the design system to tailwind.config.js and input.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,55 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      lg: "1080px",
+    },
+    container: {
+      center: true,
+    },
+    colors: {
+      black: "#000",
+      white: "#FFF",
+      gray: {
+        50: "#F6F6F6",
+        100: "#EDF2F7",
+        900: "#171923",
+        button: "#E2E8F0",
+      },
+      teal: {
+        100: "#B2F5EA",
+        200: "#81E6D9",
+        300: "#4FD1C5",
+        400: "#38B2AC",
+        900: "#1D4044",
+        button: "#179BA5",
+      },
+      "gradient-left": "#031617",
+      "gradient-right": "#2A3644",
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      med: "1rem",
+      lg: "1.125rem",
+      "2xl": "1.5rem",
+      "3xl": "2rem",
+      "4xl": "3.125rem",
+    },
+    textColor: {
+      black: "#2A3644",
+      teal: "#169AA4",
+      gray: "#4A5568",
+      white: "#FFF",
+    },
+    borderWidth: {
+      1: "1px",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "ui-sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
