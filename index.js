5a7b422c6b9ddd1ec9cfa7945e1b83bdb8abdb62
ktohalloran <ktohalloran@users.noreply.github.com>
2022-11-21 15:32:09 +0000

    deploy: c6592ca508b336b6825ce6bd77ecb20141b498bf

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-const heroText = [
-  "measuring traffic density_",
-  "understanding crop health_",
-  "tracking forest loss_",
-  "cell nuclei detection_",
-  "forecasting rip currents_",
-  "prediction of hourly global solar irradiation_",
-  "assessing storm damage_",
+const heroTextAndImage = [
+  {
+    text: "measuring traffic density_",
+    src: "images/hero-trafficdensity.jpg",
+    alt: "Traffic on a highway interchange",
+  },
+  {
+    text: "understanding crop health_",
+    src: "images/hero-crophealth2.jpg",
+    alt: "Aerial view of fields of crops",
+  },
+  {
+    text: "tracking forest loss_",
+    src: "images/hero-forestloss.jpg",
+    alt: "Color-coded map reflecting forest loss",
+  },
+  {
+    text: "cell nuclei detection_",
+    src: "images/placeholder.svg",
+    alt: "Close up of a cell nucleus",
+  },
+  {
+    text: "forecasting rip currents_",
+    src: "images/hero-ripcurrents.jpg",
+    alt: "Aerial view of a hurricane",
+  },
+  {
+    text: "prediction of hourly global solar irradiation_",
+    src: "images/hero-solarirradiation.jpg",
+    alt: "Satellite imagery of a solar farm",
+  },
+  {
+    text: "assessing storm damage_",
+    src: "images/hero-stormdamage.jpg",
+    alt: "A flooded suburban neighborhood",
+  },
 ];
 
 function changeHeroTextAndImage(moveForward) {
   // find visible image id
-  const currentImgElem = document.getElementsByClassName("in-view")[0];
+  const currentImgElem = document.getElementsByClassName("primary")[0];
   const currentIndex = parseInt(currentImgElem.id.split("-")[2]);
   // determine direction and then use mod operator to wrap if needed
   const increment = moveForward ? 1 : -1;
   const nextIndex =
-    (currentIndex + increment + heroText.length) % heroText.length;
-  // get and show next image
-  const nextImgElem = document.getElementById(`hero-img-${nextIndex}`);
-  nextImgElem.classList.add("in-view");
+    (currentIndex + increment + heroTextAndImage.length) %
+    heroTextAndImage.length;
+  // get second slot for setting new image and fade in
+  const nextImgElem = document.getElementsByClassName("secondary")[0];
+  nextImgElem.id = `hero-img-${nextIndex}`;
+  nextImgElem.src = heroTextAndImage[nextIndex].src;
+  nextImgElem.alt = heroTextAndImage[nextIndex].alt;
   $(`#hero-img-${nextIndex}`).fadeIn();
   // remove current image after addition to prevent flickering
-  currentImgElem.classList.remove("in-view");
   $(`#hero-img-${currentIndex}`).fadeOut();
+  // switch primary and secondary slots
+  nextImgElem.classList.add("primary");
+  nextImgElem.classList.remove("secondary");
+  currentImgElem.classList.add("secondary");
+  currentImgElem.classList.remove("primary");
   // replace hero text to match new slide
   const textElem = document.getElementById("hero-text");
-  textElem.innerText = heroText[nextIndex];
+  textElem.innerText = heroTextAndImage[nextIndex].text;
 }
